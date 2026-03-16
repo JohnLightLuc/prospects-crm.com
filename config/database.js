e37b9bf85002045ea7692c6b51cdc6081eb4e852
John <lamagniguitoure@gmail.com>
2026-03-16 09:01:51 +0000

    feat: set neon db

--- config/database.js
+++ config/database.js
@@ -8,9 +8,10 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 10000,
 });
 
 pool.on('error', (err) => {
